23869b0ef1c3122fc272e0cb348d401d85da8406
Enrique Delgado <rdelgado@unimercio.com>
2014-11-20 17:51:05 -0500

    Create const.php

---
+++ const.php
@@ -0,0 +1,28 @@
+<?php
+
+define("TWITTER_HANDLE","UFOTipper");
+
+define("API_KEY","xxxxxxxxxxxxxxxxx");
+define("API_SECRET","xxxxxxxxxxxxxxxxxxx");
+
+define("TOKEN_KEY","xxxxxxxxxxxxxxxxxxx");
+define("TOKEN_SECRET","xxxxxxxxxxxxxxxxxxxxx");
+
+define("HOMEPAGE","http://tipper.ufocoin.co/tips/index.php?with=true&deposit=false");
+//define("HOMEPAGE","http://ufocoin.co/tipper-3?with=true&deposit=false");
+
+define("CC_NAME","UFO Coin");
+define("CC_SYMBOL","UFO");
+
+define("MIN_CONF",3);
+define("MIN_SEND",1);
+define("MIN_WITH",10);
+
+define("RPC_USER","xxxxx");
+define("RPC_PASS","xxxxxxxxxxxxxxx");
+define("RPC_ADDR","localhost");
+define("RPC_PORT","9888");
+
+define("TWEET_QUERY","%40" . TWITTER_HANDLE . "%20OR%20%23" . TWITTER_HANDLE);
+
+?>
