5eba1c0c10c581a80c46cd1eca0649296493948e
Enrique Delgado <rdelgado@unimercio.com>
2014-11-20 19:08:53 -0500

    Create index_faq.php

---
+++ index_faq.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>UFO Coin Tipper</title>
+
+    <!-- Bootstrap core CSS -->
+
+<link href="../bootstrap/bootstrap-3.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="../bootstrap/bootstrap-3.3.0/docs/examples/jumbotron-narrow/jumbotron-narrow.css" rel="stylesheet">
+<link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+
+   </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header">
+      <ul class="nav nav-pills pull-right" role="tablist">
+          <li role="presentation"><a href="http://tipper.ufocoin.co/tips/index_home.php">Home</a></li>
+          <li role="presentation"><a href="http://tipper.ufocoin.co/tips/index.php?with">Withdraw</a></li>
+          <li role="presentation"><a href="http://tipper.ufocoin.co/tips/index.php?dep">Deposit</a></li>
+          <li role="presentation"><a target="_blank" href="http://ufocoin.co/tips">How it Works</a></li>
+      </ul>
+          <h3 class="text-muted">UFO Tipper</h3>
+       </div>
+
+    <div class="container">
+
+      <div class="jumbotron">
+        <h2>Send UFO Coins via Tweets</h2>
+        <p class="lead">@Tsoukalos great episode !!! sent you 500 UFO @ufoTipper</p>
+        <p><a class="btn btn-lg btn-success" target="_blank" href="http://ufocoin.co/tips" role="button">Learn More</a></p>
+      </div>
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+ 
+
+
+      <div class="footer">
+        <p>Sponsored by &copy; UFO Coin 2014 <a target="_blank" href="http://ufocoin.co">Learn More about UFO Coin</a>
+      </div>
+
+    </div> <!-- /container -->
+    
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
